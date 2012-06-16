f4afa0fbfce6e3fbe6faca1e5cfe98420087bce9
David M. Chandler <turbomanage@gmail.com>
2012-06-16 10:12:50 -0400

    Issue 1: added HttpPut constructor

--- src/com/turbomanage/httpclient/HttpPut.java
+++ src/com/turbomanage/httpclient/HttpPut.java
@@ -8,6 +8,23 @@ package com.turbomanage.httpclient;
 public class HttpPut extends HttpRequest {
 
     /**
+     * Constructs an HTTP PUT request with name-value pairs to
+     * be sent in the request BODY.
+     * 
+     * @param path Partial URL
+     * @param params Name-value pairs to be sent in request BODY
+     */
+    public HttpPut(String path, ParameterMap params) {
+        super();
+        this.httpMethod = HttpMethod.PUT;
+        this.path = path;
+        this.contentType = URLENCODED;
+        if (params != null) {
+            this.content = params.urlEncodedBytes();
+        }
+    }
+    
+    /**
      * Constructs an HTTP PUT request with arbitrary content.
      * 
      * @param path Partial URL
